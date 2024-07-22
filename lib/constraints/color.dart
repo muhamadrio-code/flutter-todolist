e59b825d42cd3d8f851414b325b01c8a99738121
muhammad rio <muhammad.rioperm@gmail.com>
2024-07-22 19:05:10 +0700

    Initial commit

---
+++ lib/constraints/color.dart
@@ -0,0 +1,7 @@
+import 'dart:ui';
+
+const Color tdRed = Color(0xFFDA4040);
+const Color tdBlue = Color.fromARGB(255, 44, 29, 245);
+const Color tdBlack = Color.fromARGB(255, 24, 24, 24);
+const Color tdGrey = Color.fromARGB(255, 90, 90, 90);
+const Color tdBgColor = Color(0xFFEEEFF5);
